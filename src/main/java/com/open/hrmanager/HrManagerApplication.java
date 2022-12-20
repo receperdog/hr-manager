2a2daf580e4ef68d7d8ecb16f138c60174b13093
Recep Tayyip Erdogan <receptayyiperdogan8@gmail.com>
2022-12-20 10:29:01 +0300

    Model mapper configuration completed

--- src/main/java/com/open/hrmanager/HrManagerApplication.java
+++ src/main/java/com/open/hrmanager/HrManagerApplication.java
@@ -1,13 +1,19 @@
 package com.open.hrmanager;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HrManagerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HrManagerApplication.class, args);
+    }
+    @Bean
+    public ModelMapper getModelMapper(){
+        return new ModelMapper();
     }
 
 }
