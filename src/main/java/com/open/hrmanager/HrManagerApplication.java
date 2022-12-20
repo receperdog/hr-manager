ca877e99197be760174373b3dbb34f600af87b7c
Recep Tayyip Erdogan <receptayyiperdogan8@gmail.com>
2022-12-21 01:38:32 +0300

    Modelmapper matching strategy configured

--- src/main/java/com/open/hrmanager/HrManagerApplication.java
+++ src/main/java/com/open/hrmanager/HrManagerApplication.java
@@ -1,6 +1,7 @@
 package com.open.hrmanager;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -13,7 +14,9 @@ public class HrManagerApplication {
     }
     @Bean
     public ModelMapper getModelMapper(){
-        return new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
     }
 
 }
